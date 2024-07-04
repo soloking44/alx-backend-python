0a2ccda834badd4ebb834512d8b4452533e64e04
soloking44 <solop8483@gmail.com>
2024-07-04 11:30:22 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""It contains process that returns the value of a float"""
+
+
+def to_str(n: float) -> str:
+    """
+    It changes the value to a string.
+    """
+
+    return str(n)
