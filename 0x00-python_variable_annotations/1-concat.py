fc5736dccca6585b3d44f767f366dda86d7faf61
soloking44 <solop8483@gmail.com>
2024-07-04 11:02:27 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""It ontains a process that concatenates two string values."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    it concatenates two string values.
+
+    Args:
+    - str1 (str): first string value to concatenate
+    - str2 (str): second string value to concatenate
+
+    Returns:
+    - str: concatenated string value
+    """
+    return str1 + str2
