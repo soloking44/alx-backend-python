37c04ef3ec4b8b11ba602c7ba4e2d42539ab44de
soloking44 <solop8483@gmail.com>
2024-07-09 11:07:07 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This implement a coroutine named async_generator that does not accept any arguments.
+Implement a coroutine named async_generator that does not accept any arguments.
 """
 
 import asyncio
@@ -10,7 +10,7 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    This creates a sequence of 10 numbers asynchronously.
+    This creates a series of 10 numbers.
     """
     for k in range(10):
         await asyncio.sleep(1)
